c10b8059b245ace95d58395ebd632147263cecf0
Kevin Gao <kevinyanggao@gmail.com>
2021-02-16 15:40:07 -0800

    first commit

---
+++ Home_Page.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+<title>Bernal Dental Care</title>
+<meta name="description" content="Bernal Dental Care"/>
+    <meta name="keywords" content="Bernal Dental Care, Pleasanton Dentist, Family Dentist, Family Dentistry, Dental Office, Pleasanton Chinese Dentist, cavity, filling, bridge, crown, Invisalign, root canal, whitening, "/>
+<meta name="Bernal Dental Care" content="Content1"/>
+<meta name="Name2" content="Content2"/>
+<meta name="Name3" content="Content3"/>
+<meta property="og:title" content="Bernal Dental Care"/>
+<meta name="title" content="Bernal Dental Care"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="http://bernaldentalcare.com/Home_Page.php"/>
+<meta property="og:image" content="http://bernaldentalcare.com/images/5c8e2cc615dd88801cd687ac21158650_6lj4.png"/>
+<meta property="og:image" content="http://bernaldentalcare.com/images/superstock_1071r-3709.medium_825m.jpg"/>
+<meta property="og:site_name" content="bernaldentalcare"/>
+<meta property="og:description" content="Check out http://bernaldentalcare.com!  Bernal Dental Care"/>
+<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />
+<meta http-equiv='Content-Location' content='Home_Page.php' />
+<meta name="generator" content="Starfield Technologies; Go Daddy Website Builder v6.1.5"/>
+<style type="text/css">
+body { margin:0; padding:0; }
+</style>
+<script type="text/javascript" src="scripts/imageSwap.js?2101060859"></script><script type="text/javascript">		MM_preloadImages('images/5c8e2cc615dd88801cd687ac21158650_6lj4.png');
+
+</script>
+
+
+
+
+
+
+
+<script type="text/javascript" src="//img3.wsimg.com/starfield/jquery/v1.7.2/jquery.js"></script>
+<script type="text/javascript" src="//imagesak.secureserver.net/js/jquery/jquery-ui-1.8.12.min.js"></script>
+<script type="text/javascript" src="plugins/shared/swfobject.js?2101060859"></script>
+<script type="text/javascript" src="plugins/jquery.wst.album/jquery.opf.slideshow.js?2101060859"></script>
+<script type="text/javascript" src="plugins/jquery.wst.album/jquery.wst.album.js?2101060859"></script>
+<script type="text/javascript" src="plugins/jquery.fancybox/jquery.fancybox-1.3.4.js?2101060859"></script>
+<script type="text/javascript" src="plugins/jquery.wst.background/jquery.wst.background.js?2101060859"></script>
+<link rel="stylesheet" type="text/css" id="font-junglefever.min.css" href="/dynamicfonts/style/font-JungleFever.min.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="jquery.wst.album.css" href="plugins/jquery.wst.album/jquery.wst.album.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="jquery.fancybox-1.3.4.css" href="plugins/jquery.fancybox/jquery.fancybox-1.3.4.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="layoutcontainer.css" href="layoutcontainer.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="theme.css" href="theme.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="color_1.css" href="color_1.css?2101060859" />
+<link rel="stylesheet" type="text/css" id="custom.css" href="custom.css?2101060859" />
+    
+</head>
+
+<body id='page_fc2775c6-b8c4-42da-8666-40cf46562c2a' >
+<img src="images/superstock_1071r-3709.medium_825m.jpg" id="ss-background" style="position: fixed; z-index: -1000;" class="bg-fit x-center y-middle"/><div style="position: absolute; z-index: -999; right: 0; top: 0; left: 0; bottom: 0; background-color: #fff; opacity: 0; filter: alpha(opacity=0);"></div>
+
+
+<div class="sf_outer_wrapper">
+	
+	<!-- 1 --><div class="sf_extra1"><span></span></div>
+	
+	<div class="sf_wrapper"><div style='display:block;overflow:visible;margin:0;padding:0;'><div class='sf_undocked' style='display:block;height:0;position:absolute;margin:0;padding:0;overflow:visible;width:0;top:0;z-index:999998;left:auto;'>
+<div style='display:block;position:absolute;left:707px;top:-21px;width:245px;height:162px;z-index:999980;'><div style='display:block;position:absolute;top:16px;left:10px;width:225px;height:130px;overflow:auto;'><font color="#ff0000">
+<p style="text-align: right;">
+<font color="#7f0000" style="font-size: 16px;"><strong>3283 Bernal Avenue, Suite 108<br />
+Pleasanton, CA 94566</strong></font></p>
+<font color="#7f0000">
+</font>
+<p style="text-align: right;"><font color="#7f0000" style="font-size: 16px;">
+</font></p>
+<font color="#7f0000">
+</font>
+<p style="text-align: right;"><font color="#ff0000"><font style="font-size: 20px;"><font style="font-size: 16px;"><strong><font color="#7f0000"><font color="#7f0000"><font style="font-size: 16px;">Tel:&nbsp; (925) 249 - 9242 <br />
+Fax:&nbsp;(925) 249 - 9240</font><br />
+</font>
+</font>
+</strong></font></font>
+</font></p>
+</font></div></div>
+<div style='display:block;position:absolute;left:318px;top:68px;width:383px;height:54px;z-index:999980;'><div style='display:block;position:absolute;top:16px;left:10px;width:363px;height:22px;overflow:auto;'><marquee behavior="alternate"><strong><font color="#00007f" style="font-size: 16px;">the secret to a healthy smile</font></strong></marquee></div></div>
+</div></div>
+
+	
+		<div class="sf_navigation_top">
+			<!-- Nav:Begin -->
+			
+			<!-- Nav:End -->
+	    </div><!-- /sf_navigation_top -->
+
+		<!-- 10 --><div class="sf_extra10"><span></span></div>
+
+		<div class="sf_header_wrapper">
+
+			<!-- 2 --><div class="sf_extra2"><span></span></div>
+
+			<div class="sf_main_header">
+				<!-- MainHead:Begin -->
+				<div style="display:block;background-color: rgb(229, 241, 252);margin: 0px;padding: 0px;" ><font style="font-size: 13px;"><font style="font-size: 13px;"><font style="font-size: 13px;"><font color="#7f3f00">&nbsp;</font><font style="font-size: 13px;"><font style="font-size: 13px;"><font style="font-size: 13px;"><font color="#7f0000"><font style="font-size: 18px;"><font style="font-size: 28px;"><font face="JungleFever"><font style="font-size: 22px;"><font style="font-size: 32px;">BERNAL </font></font><font face="JungleFever" style="font-size: 22px;"><font style="font-size: 32px;">DENTAL CARE</font></font></font></font></font><br />
+<font face="JungleFever" style="font-size: 22px;"><font face="JungleFever"><font style="font-size: 28px;"><font style="font-size: 18px;"><font style="font-size: 16px;"> <em>General &amp; cosmetic</em></font> </font></font><font style="font-size: 18px;"><font style="font-size: 28px;"><font face="JungleFever" style="font-size: 16px;"><em>Dentistry</em></font></font></font></font></font></font><br />
+&nbsp;<font style="font-size: 28px;"><font style="font-size: 18px;"><font style="font-size: 28px;"><font style="font-size: 18px;"></font></font></font></font>&nbsp;<br />
+&nbsp;&nbsp;&nbsp;<br />
+<br />
+</font></font></font></font></font></font></div>
+				<!-- MainHead:End -->
+			</div>
+
+			<!-- 3 --><div class="sf_extra3"><span></span></div>
+
+			<div class="sf_sub_header">
+				<!-- SubHead:Begin -->
+				<div style="display:block;" >Your Subtitle text</div>
+				<!-- SubHead:End -->
+			</div>
+
+		</div><!-- /sf_header_wrapper -->
+
+		<!-- 4 --><div class="sf_extra4"><span></span></div>
+
+		<div class="sf_navigation">
+			<!-- Nav:Begin -->
+			<div style="display:block;" >
+<ul id="Nav1" class="nav">
+<li class="active-nav sf_first_nav_item"><a href="Home_Page.php" href-publish=Home_Page.php target="_self" onclick="">Home</a></li>
+<li class=""><a href="Services.html"  target="_self" onclick="">Services</a></li>
+<li class=""><a href="Contact_Us.html"  target="_self" onclick="">Contact Us</a></li>
+<li class=""><a href="Promotion.html"  target="_self" onclick="">Promotions</a></li>
+<li class=""><a href="hours.html"  target="_self" onclick="">hours</a></li>
+<li class=" sf_last_nav_item"><a href="direction.html"  target="_self" onclick="">direction</a></li>
+</ul>
+</div>
+			<!-- Nav:End -->
+		</div><!-- /sf_navigation -->
+
+		<!-- 5 --><div class="sf_extra5"><span></span></div>
+
+		<div class="sf_pagetitle">
+			<!-- PageTitle:Begin -->
+			<div style="display:block;" ><font color="#ff0000"></font></div>
+			<!-- PageTitle:End -->
+		</div>
+
+		<!-- 6 --><div class="sf_extra6"><span></span></div>
+
+		<div class="sf_subnavigation">
+            <!-- Nav:Begin -->
+            
+            <!-- Nav:End -->
+        </div><!-- /sf_subnavigation -->
+
+        <!-- 11 --><div class="sf_extra11"><span></span></div>
+		
+		<div class="sf_content">
+			<!-- Content:Begin -->
+			<div class="LayoutContainer" style="display:block;" ><div style="display:block;" class="clearfix" ><div style="display:block; float: left; overflow: hidden; width:24.56%;"><div class="ColumnMarginBox ColumnFirst"><div style="display:block;overflow:hidden;margin: 0px;padding: 0px;" ><p style="text-align: center;">
+<br />
+<br />
+<br />
+<a href="Promotion.html"><img alt="" width="280" height="283" style="border-width: 0px; border-style: solid; width: 219px; height: 208px;" src="images/5c8e2cc615dd88801cd687ac21158650_6lj4.png"   /></a><font style="font-size: 16px;"><font style="font-size: 18px;"><font style="font-size: 18px;"><font color="#ff0000"><font style="font-size: 18px;"><font color="#7f0000"><font style="font-size: 18px;"><br />
+<strong>
+</strong></font><font style="font-size: 14px;"><strong><br />
+<font color="#56aaff">
+$79 Cleaning<br />
+$2,999 Clear Braces<br />
+$2,999 Implant</font></strong><br />
+</font>
+<br />
+<strong>
+</strong><font style="font-size: 18px;"><br />
+<strong>
+<br />
+<font color="#003f7f">
+</font><font color="#7f0000">Call Today!</font></strong><br />
+<strong>
+(925) 249-9242</strong></font></font></font><br />
+<br />
+</font></font><br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+</font></font></p></div></div></div><div style="display:block; float: left; overflow: hidden; width:75.44%;"><div class="ColumnMarginBox ColumnLast"><div style="display:block;border-color: rgb(0, 0, 0);border-style: double;border-width: 4px;margin: 0px;padding: 0px;" ><noscript>Javascript must be enabled to view the Photo Album on this page.</noscript>
+<div id="wstAlbum8fa9e4ce29ce4010999a334aa8157446">
+cache/wst.opf.3046234.xml</div>
+<script language='javascript' type='text/javascript'>
+  var url = jQuery('#wstAlbum8fa9e4ce29ce4010999a334aa8157446').text();
+  var presets8fa9e4ce29ce4010999a334aa8157446 = { fx: 'slide', columns: null, rows: null, fxShuffle: false, fxTime: 800, moveTime: 500, moveFx: 'slide', imageHeight: 200, prevNext: true, firstLast: true, wrap: true, shuffle: false, showStatus: true, slideShow: true, slideShowStart: true, slideShowDelay: 4000, slideShowContinuous: true, thumbnails: false, thumbnailsPosition: 'bottom', thumbnailsSize: 64, titleShow: false, menuFadeEnabled: true, menuPosition: 'bottom', menuCompact: true, sliderBar: false };
+  var extras8fa9e4ce29ce4010999a334aa8157446 = { __int: 0, emptyAlbumHtml: null, resources: [{height: 90},{height: 300},{height: 1200}], show: { title: false }, titleShow: false, fx: '', slideShowStart: true, slideShowDelay: 2000, imageHeight: 250, menuVisible: false, paplayer_path: 'plugins/jquery.wst.album/paplayerSlideShow.swf' };
+  presets8fa9e4ce29ce4010999a334aa8157446 = $.extend(true, presets8fa9e4ce29ce4010999a334aa8157446, extras8fa9e4ce29ce4010999a334aa8157446);
+  $('#wstAlbum8fa9e4ce29ce4010999a334aa8157446').wstAlbum(url, presets8fa9e4ce29ce4010999a334aa8157446);
+</script>
+</div><div style="display:block;overflow:hidden;" ><strong><font color="#ff0000" style="font-size: 14px;"></font></strong></div><div style="display:block;overflow:hidden;background-color: rgb(232, 241, 252);border-color: rgb(0, 0, 0);border-style: double;border-width: 4px;margin: 0px;padding: 15px;" ><p style="text-align: left;"><font size="4"><font color="#005fbf"><strong><font color="#7f0000" style="font-size: 20px;">About Us</font><br />
+</strong><br />
+<br />
+</font><font style="font-size: 16px;"><font color="#4c4c4c" style="font-size: 16px;">Bernal Dental Care is located in the city of Pleasanton, CA, but serves patients from the entire Bay Area. Newly renovated in 2012, with high-tech equipment such as digital X-Ray, 3D Panoramic, Intro-Oral camera, Bernal Dental Care proudly provides exceptional care for children, adults, and seniors.<br />
+<br />
+With over 20 years of experience in research and patient care, Dr. Gao is one of the top Dentists in America voted by the Consumers' Research Council of America. He has won numerous awards for his contribution to the field. A dedicated professional with a gentle touch, he listens to your desires to achieve the perfect smile that fits your budget. He encourages you to call him for questions regarding dental health even if you aren't a patient!<br />
+<br />
+Our dedicated staff work together to provide you with high-quality comprehensive care.</font></font><font color="#4c4c4c"><font style="font-size: 16px;">&nbsp;Use the discount coupons and visit us to discover a personalized and attentive dental experience for yourself!<br />
+</font> </font></font>
+</p>
+<ul>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">20 years of&nbsp;dental experience.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">High-tech equipment: Digital X-ray, 3D Panoramic, and Intra-Oral camera.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">24/7 emergency care available.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">Early morning, late evening, weekend appointments available upon request.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">Walk-in and same-day appointments based on availability.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">Most&nbsp;dental insurances&nbsp;accepted.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">0% interest&nbsp;financial support via CareCredit if qualified.</font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">Minimum of 5 years free maintenance for all major treatments such as root canal, crown, veneer, bridge, implant, braces, denture.</font>
+    </li>
+    <li>
+    <font size="4"><font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 16px;"><font color="#4c4c4c"><font style="font-size: 16px;">Special discount for cash patients, seniors, and new patients</font>.</font></font></font></font></font></font>
+    </li>
+    <li>
+    <font color="#4c4c4c" style="font-size: 16px;">English, Spanish, and Chinese Spoken.</font>
+    </li>
+</ul>
+<p>
+<font size="4"><font color="#4c4c4c" style="font-size: 16px;">&nbsp;&nbsp;&nbsp;<br />
+</font> <font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 16px;"><font color="#4c4c4c"><font style="font-size: 16px;">Excellent Services, Affordable Cost!<br />
+Experience the difference today!</font><br />
+<br />
+</font></font></font></font></font></font>
+</p>
+<font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 16px;"><font color="#4c4c4c"><font style="font-size: 16px;"><strong><font style="font-size: 16px;"><font style="font-size: 16px;">Contact us to make an appointment</font>&nbsp;today!</font></strong><br />
+</font><br />
+</font></font></font></font></font> <font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 14px;"><font style="font-size: 16px;"><font color="#4c4c4c"><font style="font-size: 16px;">Tel: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;(925)249-9242</font><br />
+<font style="font-size: 16px;">Fax:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; (925)249-9240</font><br />
+<font style="font-size: 16px;">E-mail: &nbsp;&nbsp;&nbsp; <a href="mailto:bernaldentalcare@gmail.com"><font color="#333333" style="font-size: 16px;">bernaldentalcare@gmail.com</font></a></font><br />
+</font></font></font></font></font> <font size="4"><font color="#4c4c4c" style="font-size: 16px;">Website:&nbsp;&nbsp;</font><a href="http://www.bernaldentalcare.com"><font color="#4c4c4c" style="font-size: 16px;">www.bernaldentalcare.com</font></a><br />
+</font><br />
+<br />
+<font size="4"><br />
+<br />
+<br />
+<br />
+</font></div></div></div></div><div><div style="display:block;overflow:hidden;" ></div></div></div>
+			<!-- Content:End -->
+		</div><!-- /sf_content -->
+
+		<!-- 12 --><div class="sf_extra12"><span></span></div>
+        
+        <div class="sf_subnavigation2">
+            <!-- Nav:Begin -->
+            
+            <!-- Nav:End -->
+        </div><!-- /sf_subnavigation2 -->
+		
+		<!-- 7 --><div class="sf_extra7"><span></span></div>
+
+		<div class="sf_footer">
+			<!-- Footer:Begin -->
+			<div style="display:block;margin: 0px;padding: 0px;" ><div style="text-align: center; display: block;"><font color="#212149" style="font-size: 12px;"><strong>content copyright <div data-wst-duda="hide"  style='display:inline;'><script type="text/javascript">
+//<![CDATA[
+    if (typeof(getCopyrightDate)=='function') document.write(getCopyrightDate(2012, null, '-')); else document.write((new Date()).getFullYear());
+    //]]>
+    </script></div>. BERNALDENTALCARE.COM. All rights reserved.</strong></font>
+</div></div>			
+			<!-- Footer:Begin -->
+		</div><!-- /sf_footer -->
+
+		<!-- 8 --><div class="sf_extra8"><span></span></div>
+
+	</div><!-- /sf_wrapper -->
+
+	<!-- 9 --><div class="sf_extra9"><span></span></div>
+
+</div><!-- /sf_outer_wrapper -->
+
+<div class="sf_banner">
+	<div style="display:block;background-color: rgb(25, 60, 142);margin: 0px;padding: 0px;" ><div style="text-align:center"><a href='http://www.godaddy.com/hosting/website-builder.aspx?isc=wscfwst304' target='_blank' rel="nofollow"><img id='bannerImage' src="images/gd_black.gif" alt='Website Builder' title='Website Builder' border='0'/></a></div></div>
+</div>
+			
+   
+</body>
+</html>
